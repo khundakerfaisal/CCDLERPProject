e75d105e0ec2c16c0ecd0c5e0e048b78b0b4c7d8
khundakerfaisal <khundakermis@gmail.com>
2025-10-14 19:26:02 +0600

    added Local po with approval

--- Pages/LocalPO.js
+++ Pages/LocalPO.js
@@ -6,14 +6,14 @@ class LocalPoPage {
         this.orderRootMenu = page.getByRole('button', { name: 'Orders' })
         this.localPoSubMenu = page.getByRole('menuitem', { name: 'Local Purchase' })
         // this.localPoFirstItem = page.locator('.o_data_row').first()
-         this.createNewLocalPo = page.getByRole('button', { name: 'New' })
-         this.selectPurchaseBase = page.getByRole('combobox', { name: 'Purchase Base' })
-        // this.createButton = page.getByRole('button', { name: 'Compare' })
-        // this.editButton = page.getByRole('button', { name: 'Edit' })
-        // this.awardedQty = page.locator('.o_purchase_selected_qty').first()
-        // this.saveButton = page.getByRole('button', { name: 'Save' })
-        // this.sendForApprovalButton = page.locator('#btn_send_for_approval');
-        // this.ApprovedButton = page.getByRole('button', { name: 'Approve', exact: true })
+        this.createNewLocalPo = page.getByRole('button', { name: 'New' })
+        this.selectPurchaseBase = page.getByRole('combobox', { name: 'Purchase Base' })
+        this.selectCS = page.getByRole('textbox', { name: 'CS' })
+        this.selectSupplier = page.getByRole('textbox', { name: 'Supplier' })
+         this.saveButton = page.getByRole('button', { name: 'Save' })
+        this.submitButton = page.getByRole('button', { name: 'Submit' })
+        this.confirmOrderButton = page.getByRole('button', { name: 'Confirm Order' })
+
 
     }
     async CCLLocalPo() {
@@ -23,29 +23,31 @@ class LocalPoPage {
         await this.localPoSubMenu.click();
         await this.createNewLocalPo.click();
         await this.page.waitForTimeout(1000);
-                await this.selectPurchaseBase.click();
-                   await this.page.keyboard.press('ArrowDown');
+        await this.selectPurchaseBase.click();
+        await this.page.keyboard.press('ArrowDown');
 
-        await this.page.keyboard.press('Enter'); 
-                        await this.page.waitForTimeout(1000);
+        await this.page.keyboard.press('Enter');
+        await this.page.waitForTimeout(1000);
 
-        // await this.createButton.click();
-        // await this.page.waitForTimeout(1000);
-        // await this.editButton.click();
-        // await this.page.waitForTimeout(1000);
-        // await this.awardedQty.fill('2');
-        // await this.page.waitForTimeout(1000);
-        // await this.saveButton.click();
-        // await this.page.waitForTimeout(4000);
-        // await this.sendForApprovalButton.waitFor({ state: 'visible', timeout: 10000 });
-        // await this.sendForApprovalButton.scrollIntoViewIfNeeded();
-        // await this.sendForApprovalButton.click();
-        // await this.page.waitForTimeout(3000);
-        // await this.ApprovedButton.click();
-        // await this.page.waitForTimeout(3000);
-        // await this.ApprovedButton.click();
-        // await this.page.waitForTimeout(3000);
-        // await this.page.pause();
+        await this.selectCS.click();
+        await this.page.waitForTimeout(1000);
+        await this.page.keyboard.press('Enter');
+        await this.page.waitForTimeout(1000);
+
+        await this.selectSupplier.click();
+        await this.page.waitForTimeout(1000);
+        await this.page.keyboard.press('Enter');
+        await this.page.waitForTimeout(1000);
+
+        await this.saveButton.click();
+        await this.page.waitForTimeout(1000);
+
+        await this.submitButton.click();
+        await this.page.waitForTimeout(1000);
+
+        await this.confirmOrderButton.click();
+        await this.page.waitForTimeout(1000);
+
 
     }
 }
