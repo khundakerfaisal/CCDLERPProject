21e2f6cf8ef62c878ac508a53b8b8c89f72955d1
khundakerfaisal <khundakermis@gmail.com>
2026-07-20 13:59:57 +0600

    added sales order and login fuctionalities

--- tests/salesOrder.spec.js
+++ tests/salesOrder.spec.js
@@ -1,23 +1,11 @@
 import { test, expect } from "@playwright/test";
+import fs from "fs";
 import LoginPage from "../Pages/LoginPage";
 import SalesOrderInputPage from "../Pages/salesOrder";
 
-// test.describe("Create CS", () => {
-
-//     test.beforeEach(async ({ page }) => {
-//         const loginPage = new LoginPage(page);
-//         await loginPage.goto();
-//         await loginPage.CCLLogin("Admin", "1234");
-//     });
-
-//     test("Create New SO", async ({ page }) => {
-//         const salesOrderPage = new SalesOrderInputPage(page);
-//         await salesOrderPage.CCDLCreateSalesOrder();
-//     });
-
-// });
-
-
+const user = JSON.parse(
+    fs.readFileSync("./tests/resources/User.json", "utf8")
+);
 test.describe.configure({ mode: "parallel" });
 
 test.describe("Create CS", () => {
@@ -25,11 +13,12 @@ test.describe("Create CS", () => {
     test.beforeEach(async ({ page }) => {
         const loginPage = new LoginPage(page);
         await loginPage.goto();
-        await loginPage.CCLLogin("Admin", "1234");
+        // await loginPage.CCLLogin(user.pilotUsername, user.pilotPassword);         // Use this line for pilot server login
+        await loginPage.CCLLogin(user.testUsername, user.testPassword);             // Use this line for test server login
     });
 
 
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 1; i++) {
 
         test(`Create New SO - ${i}`, async ({ page }) => {
 
@@ -43,4 +32,4 @@ test.describe("Create CS", () => {
 
 });
 
-//npx playwright test tests/localPo.spec.js
+//npx playwright test tests/salesOrder.spec.js --workers=3
