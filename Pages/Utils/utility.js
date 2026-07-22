be0ce2d7aa96ecdb70010e4473e5060adff9e741
khundakerfaisal <khundakermis@gmail.com>
2026-07-22 17:26:50 +0600

    added money receipt code

--- Pages/Utils/utility.js
+++ Pages/Utils/utility.js
@@ -16,6 +16,10 @@ export function generateNumber() {
     return `AUTO-${year}${month}${day}-${hour}${minute}${second}`;
 }
 
+export function generateRandomAmount(min = 1000, max = 10000) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 
 export class AllocationNumber {
